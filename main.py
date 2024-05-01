d4ba373db03182f3dcb4f96f6c0ed7531beb246c
Sr-Paixao <henriquemfpaixao@gmail.com>
2024-04-30 21:35:24 -0300

    'first'

---
+++ main.py
@@ -0,0 +1,11 @@
+from PyQt5 import uic, QtWidgets
+
+def main():
+    print('Etec')
+
+app = QtWidgets.QApplication([])
+agenda = uic.loadUi('design.ui')
+agenda.pushButton.clicked.connect(main)
+
+agenda.show()
+app.exec()
